b026d0ddf500548ddd1c844ff1402f430f0b5340
Warren Brown <warrenb95@live.co.uk>
2021-11-25 20:31:40 +0000

    chapter 1 questions

---
+++ questions/chapter1.go
@@ -0,0 +1,130 @@
+package questions
+
+import (
+	"math"
+	"strings"
+)
+
+func isUnique(input string) bool {
+	charMap := make(map[string]int)
+	for _, char := range input {
+		if _, ok := charMap[string(char)]; ok {
+			return false
+		}
+		charMap[string(char)] = 1
+	}
+	return true
+}
+
+func checkPurmutations(input1, input2 string) bool {
+	inputMap1 := make(map[string]int)
+	inputMap2 := make(map[string]int)
+
+	for _, char := range input1 {
+		if _, ok := inputMap1[string(char)]; ok {
+			inputMap1[string(char)]++
+		}
+		inputMap1[string(char)] = 1
+	}
+
+	for _, char := range input2 {
+		if _, ok := inputMap2[string(char)]; ok {
+			inputMap2[string(char)]++
+		}
+		inputMap2[string(char)] = 1
+	}
+
+	for key, value := range inputMap1 {
+		if val, ok := inputMap2[key]; !ok || val != value {
+			return false
+		}
+	}
+
+	return true
+}
+
+func urlify(input string) string {
+	var stringBuilder strings.Builder
+
+	for i := range input {
+		if string(input[i]) != " " {
+			stringBuilder.WriteByte(input[i])
+		} else {
+			stringBuilder.WriteString("%20")
+		}
+	}
+
+	return stringBuilder.String()
+}
+
+func palindromePermutation(input string) bool {
+	// Ok so the idea here is that if len(input) > 1 then there can only
+	// be one odd count of characters.
+	// I'll also ignore spaces.
+	if len(input) < 1 {
+		return true
+	}
+
+	input = strings.ToLower(input)
+
+	charMap := make(map[string]int)
+	for _, char := range input {
+		myChar := string(char)
+		if myChar != " " {
+			if _, ok := charMap[myChar]; ok {
+				charMap[myChar]++
+			} else {
+				charMap[myChar] = 1
+			}
+		}
+	}
+
+	oddCount := 0
+	for _, value := range charMap {
+		if value%2 != 0 {
+			oddCount++
+			if oddCount > 1 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func oneAway(input, output string) bool {
+	inputMap := make(map[rune]int)
+	outputMap := make(map[rune]int)
+
+	if math.Abs(float64(len(input))-float64(len(output))) > 1 {
+		return false
+	}
+
+	errorToleration := true
+
+	for _, char := range input {
+		if _, ok := inputMap[char]; ok {
+			inputMap[char]++
+		} else {
+			inputMap[char] = 1
+		}
+	}
+	for _, char := range output {
+		if _, ok := outputMap[char]; ok {
+			outputMap[char]++
+		} else {
+			outputMap[char] = 1
+		}
+	}
+
+	for key, value := range outputMap {
+		if val, ok := inputMap[key]; !ok || val != value {
+			if !errorToleration {
+				return false
+			}
+			errorToleration = false
+		}
+	}
+
+	return true
+}
---
+++ questions/chapter1_test.go
@@ -0,0 +1,201 @@
+package questions
+
+import (
+	"testing"
+)
+
+func Test_isUnique(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "is unique",
+			args: args{
+				input: "abcdef",
+			},
+			want: true,
+		},
+		{
+			name: "is not unique",
+			args: args{
+				input: "abcddef",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isUnique(tt.args.input); got != tt.want {
+				t.Errorf("isUnique() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_checkPurmutations(t *testing.T) {
+	type args struct {
+		input1 string
+		input2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "is purmutation",
+			args: args{
+				input1: "abc",
+				input2: "cba",
+			},
+			want: true,
+		},
+		{
+			name: "is not purmutation",
+			args: args{
+				input1: " abc",
+				input2: "cba",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := checkPurmutations(tt.args.input1, tt.args.input2); got != tt.want {
+				t.Errorf("checkPurmutations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_urlify(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "book example",
+			args: args{
+				input: "Mr John Smith",
+			},
+			want: "Mr%20John%20Smith",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := urlify(tt.args.input); got != tt.want {
+				t.Errorf("urlify() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_palindromePermutation(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "book example, should return true",
+			args: args{
+				input: "tact coa",
+			},
+			want: true,
+		},
+		{
+			name: "my false example",
+			args: args{
+				input: "hello",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := palindromePermutation(tt.args.input); got != tt.want {
+				t.Errorf("palindromePermutation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_oneAway(t *testing.T) {
+	type args struct {
+		input  string
+		output string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "pale -> ple == true",
+			args: args{
+				input:  "pale",
+				output: "ple",
+			},
+			want: true,
+		},
+		{
+			name: "pales -> pale == true",
+			args: args{
+				input:  "pales",
+				output: "pale",
+			},
+			want: true,
+		},
+		{
+			name: "pale -> bale == true",
+			args: args{
+				input:  "pale",
+				output: "bale",
+			},
+			want: true,
+		},
+		{
+			name: "pale -> bake == false",
+			args: args{
+				input:  "pale",
+				output: "bake",
+			},
+			want: false,
+		},
+		{
+			name: "pale -> palers",
+			args: args{
+				input:  "pale",
+				output: "palers",
+			},
+			want: false,
+		},
+		{
+			name: "palers -> pale ",
+			args: args{
+				input:  "palers",
+				output: "pale",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := oneAway(tt.args.input, tt.args.output); got != tt.want {
+				t.Errorf("oneAway() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
